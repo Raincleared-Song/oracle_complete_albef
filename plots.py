db151edc2f8d6ad30ea0920ea96b209cda842cb1
Raincleared-Song <scy.raincleared@gmail.com>
2022-05-30 00:20:37 +0800

    feat:plot data

--- plots.py
+++ plots.py
@@ -1,3 +1,4 @@
+import math
 from matplotlib import pyplot as plt
 
 
@@ -21,11 +22,11 @@ def main():
             1.0:  '74.51 / 87.26 / 89.99 / 91.26',
         },
         0.75: {
-            0:    '0 / 0 / 0 / 0',
-            0.25: '0 / 0 / 0 / 0',
-            0.5:  '0 / 0 / 0 / 0',
-            0.75: '0 / 0 / 0 / 0',
-            1.0:  '0 / 0 / 0 / 0',
+            0:    '81.25 / 91.20 / 93.14 / 94.78',
+            0.25: '81.98 / 92.48 / 93.81 / 94.78',
+            0.5:  '82.34 / 92.48 / 93.75 / 94.54',
+            0.75: '77.18 / 89.99 / 91.81 / 93.14',
+            1.0:  '75.30 / 87.92 / 90.17 / 91.14',
         }
     }
     hit_k = [1, 5, 10, 20]
@@ -52,13 +53,21 @@ def main():
     for hid in range(len(hit_k)):
         plt.subplot(2, 2, hid + 1)
         results = new_hits[hid]
+        max_res, min_res = 0, 100
         for first_mk, (second_mks, second_results) in results.items():
+            if first_mk != 0.25:
+                continue
             plt.plot(second_mks, second_results, linestyle='-', marker='.',
                      color=colors[first_mk], label=f'{first_mk:.2f}')
+            max_res = max(max_res, max(second_results))
+            min_res = min(min_res, min(second_results))
         plt.xlabel('II.覆盖率')
         plt.ylabel(f'Hit@{hit_k[hid]}')
         plt.xticks(ticks=[0, 0.25, 0.5, 0.75, 1.0])
-        plt.legend()
+        max_tick = math.ceil(max_res)
+        min_tick = math.floor(min_res)
+        plt.yticks(ticks=range(min_tick, max_tick+1, 1 if max_tick - min_tick <= 7 else 2))
+        # plt.legend()
         plt.title(f'Hit@{hit_k[hid]}')
     plt.tight_layout()
     plt.show()
